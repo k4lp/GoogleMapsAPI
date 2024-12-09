5635b758f63097ba57ed3293a312689ed17de67f
k4lp <kalppariya1234@outlook.com>
2024-12-09 23:12:01 +0530

    m

--- GoogleMapsAPI/Models/Entities/User.cs
+++ GoogleMapsAPI/Models/Entities/User.cs
@@ -1,7 +1,5 @@
 ﻿using GoogleMapsAPI.Models.CustomValidationAttributes.UserValidationAttributes;
 
-using Microsoft.AspNetCore.Mvc;
-
 using System.ComponentModel.DataAnnotations;
 
 namespace GoogleMapsAPI.Models.Entities
@@ -12,7 +10,7 @@ namespace GoogleMapsAPI.Models.Entities
         public int Id { get; set; }
 
         [Required]
-        [StringLength(20, MinimumLength = 8, ErrorMessage = "Username must be between 8 and 20 characters")]
+        //[StringLength(20, MinimumLength = 8, ErrorMessage = "Username must be between 8 and 20 characters")]
         //[Remote(action: "CheckUsername", controller: "UserValidation", ErrorMessage = "Please use proper username.")]
         [CustomUsernameValidation]
         public string? Username { get; set; }
@@ -26,25 +24,25 @@ namespace GoogleMapsAPI.Models.Entities
 
 
         [Required]
-        [Remote(action: "CheckApiKey", controller: "UserValidation", ErrorMessage = "Enter a valid API key")]
-        [Display(Name = "API KEY for Google Maps", Description = "Your Google Maps API Key", Prompt = "Enter your API key")]
+        //[Remote(action: "CheckApiKey", controller: "UserValidation", ErrorMessage = "Enter a valid API key")]
+        //[Display(Name = "API KEY for Google Maps", Description = "Your Google Maps API Key", Prompt = "Enter your API key")]
         public string? EncryptedApiKey { get; set; }
 
-
+        [Required]
         public string? ApiHash { get; set; }
 
 
         [Required]
         [EmailAddress]
-        [Remote(action: "ValidateEmail", controller: "UserValidation", ErrorMessage = "Enter a valid email address")]
+        //[Remote(action: "ValidateEmail", controller: "UserValidation", ErrorMessage = "Enter a valid email address")]
         [DataType(DataType.EmailAddress)]
-        [Display(Name = "Email Address", Description = "Your contact email", Prompt = "name@example.com")]
+        //[Display(Name = "Email Address", Description = "Your contact email", Prompt = "name@example.com")]
         public string? Email { get; set; }
 
 
         [Required]
         [DataType(DataType.PhoneNumber)]
-        [Display(Name = "Phone Number", Description = "Your contact number", Prompt = "+91 12345-67890")]
+        //[Display(Name = "Phone Number", Description = "Your contact number", Prompt = "+91 12345-67890")]
         public string? Phone { get; set; }
 
 
